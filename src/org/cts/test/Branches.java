f78f681100ca002351c26c8a30c98da4c9490fc8
indujaudhayak <indujaudhayak@gmail.com>
2019-11-27 13:26:55 +0530

    new code

--- src/org/cts/test/Branches.java
+++ src/org/cts/test/Branches.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class Branches {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 Map<Integer,String>mp=new LinkedHashMap();
 mp.put(1,"java");
 mp.put(2,"sql");
@@ -16,7 +16,7 @@ mp.put(4,"selenium");
 mp.put(5,"oracle");
 System.out.println(mp);
 Set<Entry<Integer, String>> x = mp.entrySet();
-
+Thread.sleep(2000);
 for(Entry<Integer,String>e:x)
 System.out.println(e);	
 	
